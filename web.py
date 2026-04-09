517fdc4017c76046f89321889bea11592c933b94
sourire1006 <sts25fnvsz@privaterelay.appleid.com>
2026-04-09 11:49:10 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -1,17 +1,47 @@
 from flask import Flask, render_template,request
 from datetime import datetime
+
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# 判斷是在 Vercel 還是本地
+if os.path.exists('serviceAccountKeyv0409.json'):
+    # 本地環境：讀取檔案
+    cred = credentials.Certificate('serviceAccountKeyv0409.json')
+else:
+    # 雲端環境：從環境變數讀取 JSON 字串
+    firebase_config = os.getenv('FIREBASE_CONFIG')
+    cred_dict = json.loads(firebase_config)
+    cred = credentials.Certificate(cred_dict)
+
+firebase_admin.initialize_app(cred)
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    link = "<h1>歡迎進入徐梓恩的網站</h1>"
+    link = "<h1>歡迎進入徐梓恩的網站20260409</h1>"
     link += "<a href=/mis>課程</a><hr>"
     link += "<a href=/today>現在日期時間</a><hr>"
     link += "<a href=/me>關於我</a><hr>"
     link += "<a href=/welcome?u=徐梓恩&d=靜宜行銷數位經營>Get傳值</a><hr>"
     link += "<a href=/account>post傳值</a><hr>"
     link += "<a href=/calculate>次方根號傳值</a><hr>"
+    link += "<a href=/read>讀取Firestore資料</a><hr>"
     return link
+
+@app.route("/read")
+def read():
+    Result = ""
+    db = firestore.client()
+    collection_ref = db.collection("靜宜資管")    
+    docs = collection_ref.get()  
+    docs = collection_ref.order_by("lab",direction=firestore.Query.DESCENDING).limit(5).get()  
+    for doc in docs:         
+        Result += str(doc.to_dict()) + "<br>"    
+    return Result   
 
 @app.route("/mis")
 def course():
